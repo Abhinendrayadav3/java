25d3d33601a54a5fdeb673f80a8d63bc3eddefc5
Abhinendra Yadav <75690933+Abhinendrayadav3@users.noreply.github.com>
2020-12-19 14:36:25 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,26 @@
+package lesson1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int age = 3;
+         switch (age) {
+         case 1:
+        	 System.out.println("hey u are tiny");
+        	 break;
+        	 
+         case 2:
+        	 System.out.println("hey u are small");
+        	 break;
+         case 3:
+        	 System.out.println("hey u are cool");
+        	 break;
+        	 default:
+        		 System.out.println("u r the king");
+        	 
+         }
+      
+      
+        		  }
+}
